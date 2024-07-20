53d014f138b2cb8489cc479272d84a82d060a9d0
Reuben <reubenthomas99@gmail.com>
2024-07-20 16:26:15 +0530

    Alter user model

--- models/user.js
+++ models/user.js
@@ -2,13 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+  img_id: { type: String, required: true },
+  url: { type: String, required: true }
+})
+
 const UserSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
   last_name: { type: String, required: true, maxLength: 100 },
   email: { type: String, required: true, maxLength: 100 },
   password: { type: String, required: true, maxLength: 100},
   about: { type: String },
-  image: { type: String }
+  image: { type: ImageSchema }
 });
 
 module.exports = mongoose.model("User", UserSchema);
